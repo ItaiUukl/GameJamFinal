782905386497b74a42abc2aaf296e764fa68e690
eiloncap <eiloncap@gmail.com>
2021-12-19 11:11:29 +0200

    Added CreateEdgeCollider() to Room

--- Assets/Scripts/Rooms/Room.cs
+++ Assets/Scripts/Rooms/Room.cs
@@ -1,22 +1,48 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(CompositeCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
 public class Room : MonoBehaviour
 {
+    private GameObject child;
     // Start is called before the first frame update
     void Start()
     {
-        RoomsManager.Instance.RegisterRoom(this);
+        // RoomsManager.Instance.RegisterRoom(this);
         // TODO: implement. Generates colliders
+        CreateEdgeCollider();
+    }
+
+    private void CreateEdgeCollider()
+    {
+        var poly = GetComponent<PolygonCollider2D>();
+        var position = transform.position;
+        child = new GameObject
+        {
+            // TODO: Add layers references/string names to Utils
+            layer = LayerMask.NameToLayer("Default"),
+            transform =
+            {
+                position = position,
+                rotation = transform.rotation,
+                localScale = transform.localScale
+            }
+        };
+        // var worldPointsList = poly.points.Select(t => poly.transform.TransformPoint(t))
+        //     .Select(worldSpacePoint => (Vector2) worldSpacePoint).ToList();
+        var edgeCol = child.AddComponent<EdgeCollider2D>();
+        var listOfPoints = poly.GetPath(0).ToList();
+        listOfPoints.Add(listOfPoints[0]);
+        edgeCol.SetPoints(listOfPoints);
+        child.transform.parent = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void OnCollisionEnter(Collision other)
@@ -29,5 +55,4 @@ public class Room : MonoBehaviour
     {
         // TODO: implement
     }
-    
 }
--- Assets/Scripts/Rooms/RoomsManager.cs
+++ Assets/Scripts/Rooms/RoomsManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class RoomsManager : Singleton<RoomsManager>
 {
+    [SerializeField] private Collider2D player;
     private List<Room> rooms;
 
     // Registers a new room to 
     public void RegisterRoom(Room room)
     {
         rooms.Add(room);
+        // Physics2D.IgnoreCollision(room.GetComponent<PolygonCollider2D>(), player);
     }
 
     // Resets the current level
