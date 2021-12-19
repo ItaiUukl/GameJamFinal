9f476ad7fba657d21579e7d990ebf926b6ccc2d5
eiloncap <eiloncap@gmail.com>
2021-12-19 22:42:01 +0200

    dismissed splitting colliders ):

--- Assets/Scripts/Rooms/Room.cs
+++ Assets/Scripts/Rooms/Room.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -8,10 +7,11 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     private GameObject child;
+
     // Start is called before the first frame update
     void Start()
     {
-        // RoomsManager.Instance.RegisterRoom(this);
+        RoomsManager.Instance.RegisterRoom(this);
         // TODO: implement. Generates colliders
         CreateEdgeCollider();
     }
@@ -45,9 +45,55 @@ public class Room : MonoBehaviour
     {
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        // TODO: implement. Shuts off appropriate colliders
+        Debug.Log("collision happened");
+        // var p1 = other.GetContact(0).point;
+        // var p2 = other.GetContact(other.contactCount - 1).point;
+        // var edgeColliders = child.GetComponents<EdgeCollider2D>();
+        // var newPoints = new List<Vector2>();
+        // bool firstEdgeFractionFound = false;
+        // var collidersToAdd = new List<List<Vector2>>();
+        // foreach (var edgeCol in edgeColliders)
+        // {
+        //     for (int i = 0; i < edgeCol.pointCount - 1; ++i)
+        //     {
+        //         newPoints.Add(edgeCol.points[i]);
+        //         if (firstEdgeFractionFound) continue;
+        //         if (IsOnAB(edgeCol.points[i], edgeCol.points[i + 1], p1))
+        //         {
+        //             SplitCollider(edgeCol, i, newPoints, p1, p2);
+        //             firstEdgeFractionFound = true;
+        //         }   
+        //         if (IsOnAB(edgeCol.points[i], edgeCol.points[i + 1], p2))
+        //         {
+        //             SplitCollider(edgeCol, i, newPoints, p2, p1);
+        //             firstEdgeFractionFound = true;
+        //         }
+        //     }
+        //     collidersToAdd.Add(newPoints.ToList());
+        //     firstEdgeFractionFound = false;
+        //     newPoints.Clear();
+        // }
+        // foreach (var pts in collidersToAdd)
+        // {
+        //     child.AddComponent<EdgeCollider2D>().SetPoints(pts);
+        // }
+    }
+
+    private void SplitCollider(EdgeCollider2D edgeCol, int i, List<Vector2> newPoints, Vector2 A, Vector2 B)
+    {
+        newPoints.Add(A);
+        edgeCol.points = newPoints.ToArray();
+        newPoints.Clear();
+        newPoints.Add(edgeCol.points[i+1]);
+        newPoints.Add(B);
+    }
+
+    private bool IsOnAB(Vector2 A, Vector2 B, Vector2 C)
+    {
+        return Vector2.Dot((B - A).normalized, (C - B).normalized) <= 0f &&
+               Vector2.Dot((A - B).normalized, (C - A).normalized) <= 0f;
     }
 
     // Moves room until collision
--- Assets/Scripts/Rooms/RoomsManager.cs
+++ Assets/Scripts/Rooms/RoomsManager.cs
@@ -6,8 +6,9 @@ public class RoomsManager : Singleton<RoomsManager>
 {
     [SerializeField] private Collider2D player;
     private List<Room> rooms;
+    private HashSet<Room> sharedRooms = new HashSet<Room>();
 
-    // Registers a new room to 
+    // Registers a new room to Int
     public void RegisterRoom(Room room)
     {
         rooms.Add(room);
@@ -18,6 +19,23 @@ public class RoomsManager : Singleton<RoomsManager>
     public void ResetLevel()
     {
         rooms.Clear();
+        // TODO: implement
+    }
+    
+    public void RoomConnection(Room room)
+    {
+        sharedRooms.Add(room);
+        RecalculateRoomsCollider();
+    }
+    
+    public void RoomDisconnection(Room room)
+    {
+        sharedRooms.Remove(room);
+        RecalculateRoomsCollider();
+    }
+
+    private void RecalculateRoomsCollider()
+    {
         // TODO: implement
     }
 }
