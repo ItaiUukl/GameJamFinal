4eaefb04f5552ffe8ba8da2e6a70b72b0db58d50
ItaiUukl <itaiuukl@gmail.com>
2022-01-02 01:00:42 +0200

    Basic Player Done
    Player with variable jumping based on distances.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,44 +1,55 @@
-using System;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(SpriteRenderer))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float acceleration = 5;
-    [SerializeField] private float maxSpeed = 10;
-    [SerializeField] private float jumpForce = 10;
-    [SerializeField] private float gravity = 6;
-    [SerializeField] private float airBuffer = .5f;
-    [SerializeField] private float coyoteTime = .5f;
+    [SerializeField] private float speed = 4f;
+    [SerializeField] private float airBuffer = .1f;
+    [SerializeField] private float coyoteTime = .1f;
+    [SerializeField] private float maxJumpPeak = 1.2f;
+    [SerializeField] private float minJumpPeak = .4f;
+    [SerializeField, Min(.0001f)] private float maxPeakDistance = 1.2f;
+    [SerializeField, Min(.0001f)] private float minPeakDistance = .4f;
+    [SerializeField, Min(.0001f)] private float fallDistance = .9f;
+    
+    public bool IsGrounded { set; get;}
     
     private Rigidbody2D _rb;
     private Collider2D _collider;
     private SpriteRenderer _sprite;
     
-    private Room _currRoom = null;
+    private Room _currRoom;
     private Vector2 _velocity = Vector2.zero;
+    
+    private float _jumpForce;
+    private float _gravity;
+    
     private float _coyote;
     private float _jumpBuffer;
-    private bool _isGrounded;
     
-    private readonly Vector2 _groundDetectionOffset = new Vector2(0.1f, 0.1f);
+    private float _height; 
+    private float _distance;
+
 
     // Use this for initialization
     void Start()
     {
+        _height = maxJumpPeak;
+        _distance = maxPeakDistance;
+        UpdateForces();
+        
         _rb = GetComponent<Rigidbody2D>();
         _rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
         _rb.gravityScale = 0;
         _rb.freezeRotation = true;
+        _rb.sharedMaterial = new PhysicsMaterial2D {friction = 0};
         
         _collider = GetComponent<Collider2D>();
 
         _sprite = GetComponent<SpriteRenderer>();
-        // _rb.gravityScale = gravityScale;
     }
 
     // Update is called once per frame
@@ -50,39 +61,54 @@ public class Player : MonoBehaviour
         }
 
         float xInput = Input.GetAxis("Horizontal");
-        _velocity.x += xInput * acceleration * Time.deltaTime;
-        _velocity.x = Mathf.Clamp(_velocity.x, -maxSpeed, maxSpeed);
+        _velocity.x = xInput * speed;
         _sprite.flipX = xInput < 0;
         
-        if (_isGrounded)
+        if (IsGrounded)
         {
+            _distance = maxPeakDistance;
+            _height = maxJumpPeak;
+            UpdateForces();
+            
             if (_jumpBuffer != 0)
             {
-                _velocity.y = jumpForce;
+                _velocity.y = _jumpForce;
             }
             else
             {
-                _coyote = coyoteTime;
-
-                _velocity.x = Mathf.Lerp(_velocity.x, xInput, Time.deltaTime * 15);
+                _coyote = _velocity.y < _jumpForce ? coyoteTime : _coyote;
                 
                 if (IsJumpPressed())
                 {
-                    _velocity.y = jumpForce;
+                    _velocity.y = _jumpForce;
                     _coyote = 0;
                 }
             }
         }
         else
         {
-            _velocity.y -= gravity * Time.deltaTime;
+            _velocity.y = _rb.velocity.y;
+            if (_rb.velocity.y <= 0)
+            {
+                _distance = fallDistance;
+                _height = maxJumpPeak;
+                UpdateForces();
+            }
+            else if (IsJumpReleased())
+            {
+                _height = minJumpPeak;
+                _distance = minPeakDistance;
+                UpdateForces();
+                _velocity.y = Mathf.Min(_jumpForce, _velocity.y);
+            }
             
-            Debug.Log(_coyote);
+            _velocity.y -= _gravity * Time.deltaTime;
+             
             if (IsJumpPressed())
             {
                 if (_coyote > 0)
                 {
-                    _velocity.y = jumpForce;
+                    _velocity.y = _jumpForce;
                     _coyote = 0;
                 }
                 else
@@ -90,31 +116,11 @@ public class Player : MonoBehaviour
                     _jumpBuffer = airBuffer;
                 }
             }
-
-            if (xInput == 0)
-            {
-                _coyote = Mathf.Max(_coyote - Time.deltaTime, 0);
-            }
-
-            if (IsJumpReleased())
-            {
-                _velocity.y = jumpForce / 2;
-            }
             
-
-            _velocity.x = Mathf.Lerp(_velocity.x, xInput, Time.deltaTime * 2);
-            
+            _coyote = Mathf.Max(_coyote - Time.deltaTime, 0);
         }
         _jumpBuffer = Mathf.Max(_jumpBuffer - Time.deltaTime, 0);
-
-    }
-
-    void FixedUpdate()
-    {
-        Debug.Log(_velocity);
-        _rb.MovePosition(_rb.position + _velocity * Time.deltaTime);
-        _isGrounded = IsGrounded();
-        Debug.Log(_isGrounded);
+        _rb.velocity = _velocity;
     }
 
     public void RoomMoving(Room room)
@@ -139,6 +145,12 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void UpdateForces()
+    {
+        _jumpForce = (2 * _height * speed) / _distance;
+        _gravity = (2 * _height * speed * speed) / (_distance * _distance);
+    }
+
     private static bool IsJumpPressed()
     {
         return Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space);
@@ -147,33 +159,5 @@ public class Player : MonoBehaviour
     private static bool IsJumpReleased()
     {
         return Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.Space);
-    }
-    
-    private Vector2 GetBottomLeft()
-    {
-        Bounds colliderBounds = _collider.bounds;
-        return new Vector2(colliderBounds.center.x - colliderBounds.extents.x, 
-            colliderBounds.center.y - colliderBounds.extents.y);
-    }
-    
-    private Vector2 GetBottomRight()
-    {
-        Bounds colliderBounds = _collider.bounds;
-        return new Vector2(colliderBounds.center.x + colliderBounds.extents.x, 
-            colliderBounds.center.y - colliderBounds.extents.y);
-    }
-    
-    private Vector2 GetBottom()
-    {
-        Bounds colliderBounds = _collider.bounds;
-        return new Vector2(colliderBounds.center.x, 
-            colliderBounds.center.y - colliderBounds.extents.y - 1);
-    }
-
-    private bool IsGrounded()
-    {
-        Vector2 areaTL = GetBottomLeft() + _groundDetectionOffset;
-        Vector2 areaBR = GetBottomRight() - _groundDetectionOffset;
-        return Physics2D.BoxCast(GetBottom(), _collider.bounds.size, 0f, Vector2.down, .1f);
     }
 }
