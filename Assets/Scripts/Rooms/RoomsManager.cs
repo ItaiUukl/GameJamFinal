2c752a0426edf59a759c9abeb5882fac9a75ff8a
saarbh <saar92@gmail.com>
2022-01-10 21:11:19 +0200

    Working Linerender merge

--- Assets/Scripts/Rooms/RoomsManager.cs
+++ Assets/Scripts/Rooms/RoomsManager.cs
@@ -8,28 +8,30 @@ using UnityEngine;
 public class RoomsManager : Singleton<RoomsManager>
 {
     private GameObject _compositeHolder;
-    public GameObject Line;
+    public List<GameObject> LinesList = new List<GameObject>();
+    // list for every room collider
     private List<PolygonCollider2D> _roomPoly = new List<PolygonCollider2D>();
-    private List<Vector2> vectorarr = new List<Vector2>();
+    // list of vector2 pollider points 
+    private List<Vector2[]> collPoints = new List<Vector2[]>();
 
-    private int index = 0;
+    private CompositeCollider2D compColl;
+    //room index for ref
+    private int r = 0;
     private GameObject _line;
     private GameObject[] lr;
     private GameObject lineobj;
 
-
+    
     // Registers a new room to Int
     public void RegisterRoom(Room room)
     {
         if (!_compositeHolder)
         {    
             GenerateComposite();
-            
         }
         room.transform.SetParent(_compositeHolder.transform);
         _roomPoly.Add(room._collider);
-        GenerateLineObject(room.name);
-
+        collPoints.Add(room._collider.points);
     }
 
     private void GenerateComposite()
@@ -41,64 +43,74 @@ public class RoomsManager : Singleton<RoomsManager>
         };
         _compositeHolder.AddComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
 
-        CompositeCollider2D compColl = _compositeHolder.AddComponent<CompositeCollider2D>();
+        compColl = _compositeHolder.AddComponent<CompositeCollider2D>();
         compColl.geometryType = CompositeCollider2D.GeometryType.Outlines;
         compColl.vertexDistance = .05f;
         compColl.offsetDistance = .05f;
+
     }
-    //Input: index, create a new line renderer object and return the game object
-    private void GenerateLineObject(string roomindex)
-    {
-        _line = new GameObject
-        {
-            name = "LineRenderer " + roomindex,
-            layer = LayerMask.NameToLayer(GlobalsSO.OutlinesLayer)
-        };
-        _line.AddComponent<LineRenderer>();
-        _line.AddComponent<DrawLine>();
-    }
-    // Calling the funcation on Start, might need to call on Update aswell
+
+    // Calling the funcation on Update, might need to find a better place for that, maybe on collision with other room
     private void Update() {
-    
-        DrawPath();
+            Calculatelines();
+       
     }
         
     // Input: settings up the line renderers points
-    private void DrawPath()
+    private void Calculatelines()
     {
-        int j = 0;
-        foreach (CompositeCollider2D cc in FindObjectsOfType<CompositeCollider2D>())
-        {
-            for (int i = 0; i < cc.pathCount; i++)
+            for (int i = 0; i < compColl.pathCount; i++)
             {
-                Vector2[] points = new Vector2[cc.GetPathPointCount(i)];
-                cc.GetPath(i, points);          
-                foreach (Vector2 vector2 in points)
-                {
-                    LineInsert(vector2, j , points.Length);
-                    j++;
-                }
+                Vector2[] points = new Vector2[compColl.GetPathPointCount(i)];
+                compColl.GetPath(i, points);
+                // fill the list with transform converted points
+                Vector3[] points3 = points.Select(p => (Vector3) compColl.transform.TransformPoint(p)).ToArray();
+                LineInsert(points3, i);
             }
-        }
+            if(compColl.pathCount < LinesList.Count()){
+                for(int i = compColl.pathCount; i < LinesList.Count(); i++)
+                    LineInsert(new Vector3[]{}, i);
+            }
+        
     }
+    //points = collPoints.Select(t => (Vector2)polyColl.transform.TransformPoint(t)).ToArray();
+    //Input: point, index in the composite, room index create a new line renderer object and return the game object
+    private void LineInsert(Vector3[] points, int idx){
+        //create new line object
+        LineRenderer l;
+        if(idx >= LinesList.Count()){
+            LinesList.Add(new GameObject
+            {
+                name = "LineRenderer " + idx,
+                layer = LayerMask.NameToLayer(GlobalsSO.OutlinesLayer)
+            });
+            l = LinesList[idx].AddComponent<LineRenderer>();
+            l.loop = true;
+            l.startWidth = 0.2f;
+            l.textureMode = LineTextureMode.Tile;
+            l.sortingLayerName  = "Room";
+            l.material = Resources.Load("Materials/LineRenderRoom", typeof(Material)) as Material;
 
-    private void LineInsert(Vector2 v2, int index, int pointl){
-        foreach (LineRenderer line in FindObjectsOfType<LineRenderer>())
+        } 
+        else
         {
-            Vector3[] positions = new Vector3[8];
-            positions[index] = v2;
-            line.SetPosition(index , positions[index]);
-            line.GetComponent<LineRenderer>().positionCount = positions.Length; 
-            Debug.Log("line " + line);
-            Debug.Log("Point " + v2);
-            Debug.Log("index " + index);
+            l = LinesList[idx].GetComponent<LineRenderer>();
         }
+            l.positionCount = points.Count();
+            l.SetPositions(points);
+ 
+            
+
+    
     }
     
     // Resets the current level
     public void ResetLevel()
     {
         _compositeHolder = null;
+        LinesList = new List<GameObject>();
+        compColl = null;
+        
         // TODO: might need more
     }
 }
